08cd153fe8f8a27188cc8b0644dbb024d15ea881
rogelio.clemente <rogelioclemente23@hotmail.com>
2023-04-24 01:28:08 -0600

    Updating app component and adding a nav-bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Uni e-commerce';
+  products: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:7163/api/products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statment');
+      }
+    })
+  }
 }
